5c3f2017b0033a5a1173f6d709d13c8d39026d3e
Rihab GASMI <79666020+Rihab1996Gasmi@users.noreply.github.com>
2022-04-20 04:36:22 +0200

    Delete Forum_branch directory

--- Forum_branch/src/main/java/tn/consommiTounsi/spring/controller/IUserController.java
+++
@@ -1,5 +0,0 @@
-package tn.consommiTounsi.spring.controller;
-
-public interface IUserController {
-
-}
--- Forum_branch/src/main/java/tn/consommiTounsi/spring/model/MessageType.java
+++
@@ -1,5 +0,0 @@
-package tn.consommiTounsi.spring.model;
-
-public enum MessageType {
-	CHAT,JOIN,LEAVE
-}
